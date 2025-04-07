57af3e88705b3c1da6ee1c1224093ead1cad810c
manhdoanh04 <libyty97@gmail.com>
2025-04-07 16:27:01 +0700

    update sider

--- src/app/components/sider/Sider.tsx
+++ src/app/components/sider/Sider.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link"
 import SiderMenu from "./SiderMenu"
-
+import icon from "../../../../public/logomusic.svg"
 export const Sider=()=>
 {
     return(
@@ -8,7 +8,7 @@ export const Sider=()=>
         <div className="bg-[#212121] w-[280px] h-[100vh] fixed">
                  <div className="py-[25px] px-[20px] bg-[#1C1C1C]">
                     <Link href={"/"} className="flex justify-center items-center gap-[6.5px]">
-                    <img src="/logomusic.svg" alt="logo"
+                    <img src={icon.src} alt="logo"
                     className="h-[42px] w-[auto] "
                     />
                     <h1 className="font-[700] text-[24px] leading-[120%] text-[#00ADEF]">Music Web</h1>
